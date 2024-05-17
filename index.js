7c30155c73e21d7eb06f5c898eb7efb64a312dc3
mhamza8113 <161412617+mhamza8113@users.noreply.github.com>
2024-05-17 21:34:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const form = document.querySelector("form"),
       nextBtn = form.querySelector(".nextBtn"),
       backBtn = form.querySelector(".backBtn"),
       allInput = form.querySelectorAll(".first input"),
-      btnText = form.querySelector(".btnText");
+      btnText = form.querySelector(".submit");
 
 nextBtn.addEventListener("click", () => {
   allInput.forEach(input => {
